7fa282c8da6e81c880371a1385488308745b2643
martchello <marcello_1000@yahoo.com.br>
2017-05-05 17:54:29 -0300

    Functions to offer indic.tam.

---
+++ include/searchIndicadorTamanho.h
@@ -0,0 +1,7 @@
+#ifndef _SEARCHINDICADORTAMANHO_H_
+#define _SEARCHINDICADORTAMANHO_H_
+
+void indicadorTamanho_printDataBase(FILE *fp, int n);
+void indicadorTamanho_printRegister(FILE *fp, int n, int offset);
+
+#endif
---
+++ src/indicadorTamanho/searchIndicadorTamanho.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void indicadorTamanho_printDataBase(FILE *fp, int n) {
+
+
+}
+
+void indicadorTamanho_printRegister(FILE *fp, int n, int offset) {
+
+	
+}
