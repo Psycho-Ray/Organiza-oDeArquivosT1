77bb24ca2df921be84be60e796baa5af1dbada2e
BrunoGomesCoelho <brasinhacoelho@hotmail.com>
2017-05-06 00:44:54 -0300

    ImpressaoCampoIndicadorTamanho

---
+++ src/indicadorTamanho/searchFieldIndicadorTam.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include <string.h>
+
+#include <printTamanho.h>
+#include <utils.h>
+#include <searchFieldMain.h>
+
+void searchFieldIndicadorTam(FILE *fp, int n) {
+	t_search search;
+	bool found = false;	
+	int regSize, current_pos;
+	int state_size, dateTimeOri_size, dateTimeUpd_size, ticket, i; 
+	char *domain, *document_number, *name, *city, *state, *dateTimeOri, *dateTimeUpd;
+	
+	
+	search = searchFieldMain;
+	
+	// if the user has decided to abort
+	if (search.fieldType == INVALID)
+		return;
+	
+	
+	/* TODO: Essa não é a versão mais otimizado que poderia ser feito.
+		Se estou procurando por um domínio específico (primeiro campo), tecnicamente não precisao alocar os outros 7 
+		se estiver errado aquele domínio, apenas ler o tamanho do campo e dar fseek.
+		Mas isso envolveria implementar 8 ifs diferente, muuuitos iguais....
+		Posso deixar assim e mencionar isso no relatório? 
+	*/
+	for (i = 0; i < n; i += 1) {
+		
+		// TODO: what if this is zero?	
+		fread(&regSize, sizeof(int), 1, fp);
+		
+		// Read's the domain
+		fread(&domain_size, sizeof(int), 1, fp);
+		domain = (char *) malloc(sizeof(char) * (domain_size));
+		fread(domain, sizeof(char), domain_size, fp);
+		
+		// Read's the document number
+		fread(&document_number_size, sizeof(int), 1, fp);
+		document_number = (char *) malloc(sizeof(char) * (document_number_size));
+		fread(document_number, sizeof(char), document_number_size, fp);
+
+		// Read's the name 
+		fread(&name_size, sizeof(int), 1, fp);
+		name = (char *) malloc(sizeof(char) * (name_size));
+		fread(name, sizeof(char), name_size, fp);
+
+		// Read's the city name
+		fread(&city_size, sizeof(int), 1, fp);
+		city = (char *) malloc(sizeof(char) * (city_size));
+		fread(city, sizeof(char), city_size, fp);
+
+		// Read's the state name
+		fread(&state_size, sizeof(int), 1, fp);
+		state = (char *) malloc(sizeof(char) * (state_size));
+		fread(state, sizeof(char), state_size, fp);
+
+		// Read's the date and time when the domain was created
+		fread(&dateTimeOri_size, sizeof(int), 1, fp);
+		dateTimeOri = (char *) malloc(sizeof(char) * (dateTimeOri_size));
+		fread(dateTimeOri, sizeof(char), dateTimeOri_size, fp);
+
+		// Read's the date and time of when the file was last modified
+		fread(&dateTimeUpd_size, sizeof(int), 1, fp);
+		dateTimeUpd = (char *) malloc(sizeof(char) * (dateTimeUpd_size));
+		fread(dateTimeUpd, sizeof(char), dateTimeUpd_size, fp);
+
+		// Read the ticket number
+		fread(&ticket, sizeof(int), 1, fp);  // Indicates the size of a int
+		fread(&ticket, sizeof(int), 1, fp);  // The number itself
+		
+		if( (search.fieldType == DOMAIN && strcmp(domain, search.query)) ||
+			(search.fieldType == DOCUMENT && strcmp(document, search.query)) ||
+			(search.fieldType == NAME && strcmp(name, search.query)) ||
+			(search.fieldType == CITY && strcmp(city, search.query)) ||
+			(search.fieldType == STATE && strcmp(state, search.query)) ||
+			(search.fieldType == DATE_CREATED && strcmp(dateTimeOri, search.query)) ||
+			(search.fieldType == DATE_UPDATED && strcmp(dateTimeUpd, search.query)) ||
+			(search.fieldType == TICKET && ticket == atoi(search.query))
+		  ) {
+				printRegister(domain, document, name, city, state, dateTimeOri, dateTimeUpd, ticket);
+				found = true;
+			}
+		}
+		
+		if(!found)
+			printf("Campo não encontrado em nenhum registro do arquivo.\n");
+		printf("Retornando ao menu\n");		
+}
---
+++ src/searchFieldMain.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <utils.h>
+#include <stbbool.h>
+#include <string.h>
+#include <serchFieldMain.h>
+
+
+t_searchField searchMain() {
+	bool validInput = false;
+	int op = -1;
+	char *query, memError[100] = "em formato não válido digitado. Por favor tente de novo ou digite uma string vazia para abortar\n";
+	t_searchField search;
+	search.fieldType = INVALID;
+	
+	while( !validInput) {
+	
+		while (op >= 0 && op < 9) {
+			printf("Digite o número correspondente ao campo que quer pesquisar nos registros, ou 0 para abortar e voltar ao menu\n");
+			printf("1 - Documento\n");
+			printf("2 - Data e hora de criação\n");
+			printf("3 - Data e hora de última atualização\n");
+			printf("4 - Ticket\n");
+			printf("5 - Domínio\n");
+			printf("6 - Nome\n");
+			printf("7 - Cidade\n");
+			printf("8 - Estado\n");
+		
+			scanf("%d", &op);
+		
+			if (op < 0 || op >= 9) 
+				printf("Valor inválido digitado, por favor tente de novo.\n");
+				
+			if (op == 0)
+				return search;
+		}
+	
+		printf("Digite o campo: ");
+		search.fieldType = op;
+		search.query = readLine(stdin, '\n', '\n');
+		
+		// Verifies  if the user choose a fixed sized field, but did not write the correct amount of characters
+		if (op == DOCUMENT && strlen(query) != SIZE_DOCUMENT)
+			printf("Documento %s", memError);
+		else if ( (op == DATE_CREATED || op == DATE_UPDATED) && strlen(query) != SIZE_TIME ) 
+			printf("Data %s", memError);
+		else if ( (op == TICKET && strlen(query) != SIZE_TICKET))
+			printf("Ticket %s", memError);
+		else
+			return search;				 
+	}
+}
+
+
