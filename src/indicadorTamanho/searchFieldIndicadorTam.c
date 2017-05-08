5cd725e712f03b79fc97d17856c7de6c185f9026
BrunoGomesCoelho <brasinhacoelho@hotmail.com>
2017-05-07 23:20:19 -0300

    Erros de compilação na busca

--- src/indicadorTamanho/searchFieldIndicadorTam.c
+++ src/indicadorTamanho/searchFieldIndicadorTam.c
@@ -4,25 +4,24 @@
 #include <string.h>	// strcmp, strlen
 
 #include <utils.h>
+#include <readTamanho.h>
+
 #include <searchFieldMain.h>
 
 
 void searchFieldIndicadorTam(FILE *fp) {
 	t_searchField search;
+	t_field field;
 	bool found = false;	
 	int regSize, count = 0;
-	int domain_size, document_number_size, name_size, city_size;
-	int state_size, dateTimeOri_size, dateTimeUpd_size, ticket;
-	char *domain, *document_number, *name, *city, *state, *dateTimeOri, *dateTimeUpd;
 	
+	// Gets the field to be searched from the user
 	search = searchFieldMain();
 	
-	// if the user has decided to abort
+	// if the user has decided to abort at the end of the last function, we abort
 	if (search.fieldType == INVALID || strlen(search.query) == 0)
 		return;
-	
-	/* TODO supoe que guardamos 0 no final do arquivo quando chegamos ao fim dos registros.
-		falar com o Gabriel Cruz sobre como ele implementou. */
+
 	while (!feof(fp)) {
 	
 		// read the size of the register
@@ -36,22 +35,22 @@ void searchFieldIndicadorTam(FILE *fp) {
 		*/
 		// TODO: Supoe que guardamos o \0 na string
 
-		// Read the ticket number
-		fread(&ticket, sizeof(int), 1, fp);  // Indicates the size of a int
-		fread(&ticket, sizeof(int), 1, fp);  // The number itself
+		// reads a field
+		field = readFieldindicadorTamanho(fp);
 		
-		if( (search.fieldType == DOMAIN && strcmp(domain, search.query)) ||
-			(search.fieldType == DOCUMENT && strcmp(document_number, search.query)) ||
-			(search.fieldType == NAME && strcmp(name, search.query)) ||
-			(search.fieldType == CITY && strcmp(city, search.query)) ||
-			(search.fieldType == STATE && strcmp(state, search.query)) ||
-			(search.fieldType == DATE_CREATED && strcmp(dateTimeOri, search.query)) ||
-			(search.fieldType == DATE_UPDATED && strcmp(dateTimeUpd, search.query)) ||
-			(search.fieldType == TICKET && ticket == atoi(search.query))
+		// verifies if its the field the user is looking for, with the corrects contents
+		if( (search.fieldType == DOMAIN && strcmp(field.domain, search.query)) ||
+			(search.fieldType == DOCUMENT && strcmp(field.document_number, search.query)) ||
+			(search.fieldType == NAME && strcmp(field.name, search.query)) ||
+			(search.fieldType == CITY && strcmp(field.city, search.query)) ||
+			(search.fieldType == STATE && strcmp(field.state, search.query)) ||
+			(search.fieldType == DATE_CREATED && strcmp(field.dateTimeOri, search.query)) ||
+			(search.fieldType == DATE_UPDATED && strcmp(field.dateTimeUpd, search.query)) ||
+			(search.fieldType == TICKET && strcmp(field.ticket, search.query))
 		  ) 
 		{
-			/* TODO: Modificar a impressão par ficar bonitinho */
-			printField(name, domain, document_number, city, state, dateTimeOri, dateTimeUpd, ++count, ticket);
+			/* TODO: Modificar a impressão par ficar bonitinho, acho que o count não faz sentido */
+			printField(field, ++count);
 			found = true;
 		}
 		
@@ -60,4 +59,5 @@ void searchFieldIndicadorTam(FILE *fp) {
 		printf("Campo não encontrado em nenhum registro do arquivo.\n");
 	printf("Retornando ao menu\n");
 }
+
 
