e89fb450e4e3e801737d10cb3678baf684baf9c5
martchello <marcello_1000@yahoo.com.br>
2017-05-07 23:53:50 -0300

    Print tamanho mudado para campo unificado

--- src/indicadorTamanho/printTamanho.c
+++ src/indicadorTamanho/printTamanho.c
@@ -18,36 +18,29 @@
 
 */
 
-// TODO: Marcelo: Usar a struct t_field, vou deixar comentado enquanto isso para compilar // Bruno
-
-/*
 void size_printDataBase(FILE *fp, int n) {
-	t_field field;
-	int i;
+	t_field aux; 
+	int i, record_size; 
 
+	// Read every record
 	for (i = 0; i < n; i++) {
-//		field = readFieldindicadorTamanho(fp);
+		fread(&record_size, sizeof(int), 1, fp);
 
-		// Print the information
-		printf("\t%d - %s\n", i, field.name);
-		printf("\t\t\tDomain: \t%s\n", field.domain);
-		printf("\t\t\tDocument Number: \t%s\n", field.document_number);
-		printf("\t\t\tCity: \t%s\n", field.city);
-		printf("\t\t\tState: \t%s\n", field.state);
-		printf("\t\t\tDate and time created: \t%s\n", field.dateTimeOri);
-		printf("\t\t\tDate and time last updated: \t%s\n", field.dateTimeUpd);
-		printf("\t\t\tTicket number: \t%d\n", field.ticket);
+		// Read the fields
+		aux = readFieldindicadorTamanho(fp);
 
-	//	freeFieldIndicadorTamanho(field);
+		// Print them
+		printField(aux, i);
 	}
 
 	// Make the file pointer return to the begging of the file
 	rewind(fp);
 }
 
+
 void size_printRecord(FILE *fp, int n, int offset) {
-	t_field field;
-	int register_size, i;
+	t_field aux;
+	int record_size, i;
 
 	// If the offset isn't valid, just return
 	if (offset < 0 || offset >= n) {
@@ -57,22 +50,19 @@ void size_printRecord(FILE *fp, int n, int offset) {
 
 	// Go to the offset requested
 	for (i = 0; i < offset; i++) {
-		fread(&register_size, sizeof(int), 1, fp);
+		// Read's the record's size
+		fread(&record_size, sizeof(int), 1, fp);
 
-		// Se o contador do registro contar ele mesmo, deve ser register_size -4. 
-		//  Ver com o Cruz depois como ele implementou 
-		fseek(fp, register_size, SEEK_CUR);
+		// Skip the record
+		fseek(fp, record_size, SEEK_CUR);
 	}
 
-//	field = readFieldindicadorTamanho(fp); => JUST FOR NOT BREAKING THE CODE
+	// Read the fields
+	aux = readFieldindicadorTamanho(fp);
 
-	// Print the information
-	printField(field.name, field.domain, field.document_number, field.city, field.state, 
-				field.dateTimeOri, field.dateTimeUpd, offset, field.ticket);
+	// Print them
+	printField(aux, i);
 
-	// frees the strings
-//	freeFieldIndicadorTamanho(field);
-	
 	// Make the file pointer return to the begging of the file
 	rewind(fp);
-} */
+} 
